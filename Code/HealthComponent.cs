76784cb72a20415abbc52551c7b47e8a99e277ff
pietaMEDS <maxibanp@gmail.com>
2026-04-01 11:21:43 +0500

    add hud integration and some filebase

--- Code/HealthComponent.cs
+++ Code/HealthComponent.cs
@@ -1,14 +1,19 @@
 using Sandbox;
+using System;
 
 public sealed class HealthComponent : Component
 {
 	[Property] private float health = 20f;
     [Property] private float maxHealth = 20f;
 
+	public static event Action<float> OnDamageTaken;
+
 	public void TakeDamage(float damage)
 	{
 		health -= damage;
 		Log.Info($"{GameObject.Name} took {damage} damage! HP: {health}/{maxHealth}");
+
+		OnDamageTaken?.Invoke(damage);
 		
 		if (health <= 0)
 		{
--- Code/NPCs/ZombieSpawns.cs
+++ Code/NPCs/ZombieSpawns.cs
@@ -5,14 +5,32 @@ public sealed class ZombieSpawns : Component
 	[Property]
 	GameObject ZombiePrefab { get; set; }
 
+	[Property]
+	private float BaseCooldown = 5f;
+
 	public float lastSpawnTime = Time.Now;
 
 	protected override void OnUpdate()
 	{
-		if (Time.Now - lastSpawnTime > 5f)
+		if ( Time.Now - lastSpawnTime > BaseCooldown )
 		{
-			var zombie = ZombiePrefab.Clone( WorldPosition );
+			// Размер куба (половина, потому что центр)
+			Vector3 halfSize = WorldScale * 20f;
+
+			Log.Info( $"Spawning zombie at {WorldPosition} with half size {halfSize}" );
+
+			// Случайная позиция внутри куба
+			Vector3 randomOffset = new Vector3(
+				Game.Random.Float( -halfSize.x, halfSize.x ),
+				Game.Random.Float( -halfSize.y, halfSize.y ),
+				Game.Random.Float( -halfSize.z, halfSize.z )
+			);
+
+			Vector3 spawnPosition = WorldPosition + randomOffset;
+
+			var zombie = ZombiePrefab.Clone( spawnPosition );
+
 			lastSpawnTime = Time.Now;
-		}	
+		}
 	}
 }
